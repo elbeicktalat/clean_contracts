5ef570dcf3712c42b1955c7cdf99955105c9ef3b
Talat El Beick <talatelbeick@gmail.com>
2022-08-14 21:11:22 +0200

    export views and add build_context_extension

--- packages/flutter_clean_contracts/lib/flutter_clean_contracts.dart
+++ packages/flutter_clean_contracts/lib/flutter_clean_contracts.dart
@@ -4,3 +4,7 @@ export 'package:clean_contracts/core/data/data_source.dart';
 export 'package:clean_contracts/core/data/model.dart';
 export 'package:clean_contracts/core/domain/entity.dart';
 export 'package:clean_contracts/core/domain/repository.dart';
+export 'package:clean_contracts/core/presentation/extensions/build_context_extension.dart';
+export 'package:clean_contracts/core/presentation/view/stateful/view.dart';
+export 'package:clean_contracts/core/presentation/view/stateless/screen.dart';
+export 'package:clean_contracts/core/presentation/view/stateless/view.dart';
