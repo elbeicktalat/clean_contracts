6a492555709ce52835c11ba82b0e1a55435e3ef9
Talat El Beick <talatelbeick@gmail.com>
2022-08-14 21:05:44 +0200

    add ViewState & StateViewModel

---
+++ packages/flutter_clean_contracts/lib/core/presentation/view/stateful/view.dart
@@ -0,0 +1,86 @@
+import 'package:clean_contracts/core/presentation/view_model/state_view_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get_it/get_it.dart';
+
+/// The State mixin.
+///
+/// This allows the [StateViewModel] to deal with [State] methods.
+mixin StateMixin<T extends StatefulWidget, V extends StateViewModel>
+    on State<T> {
+  /// The instance of the ViewModel.
+  @protected
+  V get viewModel => GetIt.instance.get<V>();
+
+  /// The widget method which get the place of build method.
+  @protected
+  Widget? builder();
+
+  @override
+  Widget build(final BuildContext context) {
+    viewModel.context = context;
+    return builder()!;
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void initState() {
+    super.initState();
+    viewModel.initState();
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void reassemble() {
+    viewModel.reassemble();
+    super.reassemble();
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void deactivate() {
+    viewModel.deactivate();
+    super.deactivate();
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void activate() {
+    super.activate();
+    viewModel.activate();
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void dispose() {
+    viewModel.dispose();
+    super.dispose();
+  }
+
+  @protected
+  @override
+  @mustCallSuper
+  void didChangeDependencies() {
+    viewModel.didChangeDependencies();
+    super.didChangeDependencies();
+  }
+
+  @override
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(DiagnosticsProperty<V>('viewModel', viewModel));
+    viewModel.debugFillProperties(properties);
+  }
+}
+
+/// [ViewState] is the implementation of [State].
+///
+/// Need tow type parameter the first is subclass of [StatefulWidget]
+/// and the second is a subclass of [StateViewModel].
+abstract class ViewState<T extends StatefulWidget, V extends StateViewModel>
+    extends State<T> with StateMixin<T, V> {}
---
+++ packages/flutter_clean_contracts/lib/core/presentation/view_model/state_view_model.dart
@@ -0,0 +1,66 @@
+import 'package:clean_contracts/core/presentation/view_model/view_model.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+/// The dedicated [BaseViewModel] to deal with state of [StatefulWidget].
+class StateViewModel extends BaseViewModel {
+  /// Called when this object is inserted into the tree.
+  ///
+  /// The framework will call this method exactly once for each [State] object
+  /// it creates.
+  ///
+  /// Implementations of this method should start with a call to the inherited
+  /// method, as in `super.initState()`.
+  @mustCallSuper
+  void initState() {}
+
+  /// In addition to this method being invoked, it is guaranteed that the
+  /// [build] method will be invoked when a reassemble is signaled. Most
+  /// widgets therefore do not need to do anything in the [reassemble] method.
+  @mustCallSuper
+  void reassemble() {}
+
+  /// Called when this object is removed from the tree.
+  ///
+  /// Subclasses should override this method to clean up any links between
+  /// this object and other elements in the tree (e.g. if you have provided an
+  /// ancestor with a pointer to a descendant's [RenderObject]).
+  ///
+  /// Implementations of this method should end with a call to the inherited
+  /// method, as in `super.deactivate()`.
+  @mustCallSuper
+  void deactivate() {}
+
+  /// Called when this object is reinserted into the tree after having been
+  /// removed via [deactivate].
+  ///
+  /// Implementations of this method should start with a call to the inherited
+  /// method, as in `super.activate()`.
+  @mustCallSuper
+  void activate() {}
+
+  /// Called when this object is removed from the tree permanently.
+  ///
+  /// Implementations of this method should end with a call to the inherited
+  /// method, as in `super.dispose()`.
+  ///
+  /// See also:
+  ///
+  ///  * [deactivate], which is called prior to [dispose].
+  @mustCallSuper
+  void dispose() {}
+
+  /// Called when a dependency of this [State] object changes.
+  ///
+  /// This method is also called immediately after [initState]. It is safe to
+  /// call [BuildContext.dependOnInheritedWidgetOfExactType] from this method.
+  @mustCallSuper
+  void didChangeDependencies() {}
+
+  /// Add additional properties associated with the node.
+  ///
+  /// For more info hit on:
+  /// [Debug properties | Decoding Flutter](https://www.youtube.com/watch?v=DnC7eT-vh1k),
+  @mustCallSuper
+  void debugFillProperties(final DiagnosticPropertiesBuilder properties) {}
+}
