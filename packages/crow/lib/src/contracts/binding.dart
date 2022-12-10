56e1a189ed33407332e1ec0d4269e1964c4f9290
Talat El Beick <talatelbeick@gmail.com>
2022-12-10 22:38:03 +0100

    add instance method to binding

--- packages/crow/lib/src/contracts/binding.dart
+++ packages/crow/lib/src/contracts/binding.dart
@@ -2,6 +2,40 @@
 //  Use of this source code is governed by a MIT-style license that can be
 //  found in the LICENSE file.
 
-import 'package:get/get.dart' show Bindings;
+import 'package:get/get.dart'
+    show
+        AsyncInstanceBuilderCallback,
+        Bindings,
+        Get,
+        Inst,
+        InstanceBuilderCallback;
+import 'package:meta/meta.dart';
 
-abstract class Binding extends Bindings {}
+abstract class Binding extends Bindings {
+  @protected
+  S put<S>(
+    S dependency, {
+    String? tag,
+    bool permanent = false,
+  }) =>
+      Get.put<S>(dependency, tag: tag, permanent: permanent);
+
+  @protected
+  Future<S> putAsync<S>(
+    AsyncInstanceBuilderCallback<S> builder, {
+    String? tag,
+    bool permanent = false,
+  }) async =>
+      Get.putAsync<S>(builder, tag: tag, permanent: permanent);
+
+  @protected
+  void lazyPut<S>(
+    InstanceBuilderCallback<S> builder, {
+    String? tag,
+    bool fenix = false,
+  }) =>
+      Get.lazyPut<S>(builder, tag: tag, fenix: fenix);
+
+  @protected
+  S find<S>({String? tag}) => Get.find<S>(tag: tag);
+}
