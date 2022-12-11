bdde3c884e68d0de6385fd741a85d89fef2cea88
Talat El Beick <talatelbeick@gmail.com>
2022-12-11 20:29:04 +0100

    add async UseCase and Service

--- packages/crow/lib/src/contracts/domain/usecase.dart
+++ packages/crow/lib/src/contracts/domain/usecase.dart
@@ -5,5 +5,9 @@
 import 'package:crow/src/contracts/params.dart';
 
 abstract class UseCase<T, P extends Params?> {
+  T call(P params);
+}
+
+abstract class AsyncUseCase<T, P extends Params?> {
   Future<T> call(P params);
 }
